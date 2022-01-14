caf9348fe2edb23965ec54cb40937192665c1871
Belov Roman <rnbelov96@gmail.com>
2022-01-14 14:06:09 +0300

    Make CEO edits

--- src/ts/thanks.ts
+++ src/ts/thanks.ts
@@ -1,5 +1,9 @@
 import '../scss/thanks.scss';
 // import './modals';
 
+const userName = localStorage.getItem('userName') || 'Гость';
+
 const nameLabelEl = document.querySelector('.js-name') as HTMLSpanElement;
-nameLabelEl.textContent = localStorage.getItem('userName') || 'Гость';
+nameLabelEl.textContent = userName;
+
+document.title = `${userName}, спасибо, Ваша заявка принята 👍`;
